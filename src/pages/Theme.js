b6bb61218471a86743d7ce8a437df41b0c165e48
singcl <24661881@qq.com>
2019-03-03 18:58:04 +0800

    :art: 主题文件

---
+++ src/pages/Theme.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ * @author singcl <iambabyer@gmail.com>
+ * @see https://github.com/singcl
+ */
+
+export const themes = [
+    {
+        color: ['#db4437'],
+        name: '姨妈红',
+    },
+    {
+        color: ['#0f9d58'],
+        name: '酷安绿',
+    },
+    {
+        color: ['#fb7299'],
+        name: '哔哩粉',
+    },
+    {
+        color: ['#3f51b5'],
+        name: '颐堤蓝',
+    },
+    {
+        color: ['#009688'],
+        name: '水鸭青',
+    },
+    {
+        color: ['#ff9800'],
+        name: '伊藤橙',
+    },
+    {
+        color: ['#673ab7'],
+        name: '基佬紫',
+    },
+    {
+        color: ['#2196f3'],
+        name: '知乎蓝',
+    },
+    {
+        color: ['#795548'],
+        name: '古铜棕',
+    },
+    {
+        color: ['#607d8b'],
+        name: '低调灰',
+    },
+    {
+        color: ['#212121'],
+        name: '暮夜黑',
+    },
+]
+
+export const themesGradient: Array<{ color: Array<String> }> = [
+    {
+        color: ['#ff5858', '#f09819'],
+    },
+    {
+        color: ['#8fd3f4', '#84fab0'],
+    },
+    {
+        color: ['#f5576c', '#f093fb'],
+    },
+    {
+        color: ['#4facfe', '#00f2fe'],
+    },
+    {
+        color: ['#fa709a', '#fee140'],
+    },
+    {
+        color: ['#667eea', '#764ba2'],
+    },
+    {
+        color: ['#ff0844', '#ffb199'],
+    },
+    {
+        color: ['#b721ff', '#21d4fd'],
+    },
+    {
+        color: ['#09203f', '#537895'],
+    },
+    {
+        color: ['#16a085', '#f4d03f'],
+    },
+]
