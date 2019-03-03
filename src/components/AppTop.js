a78fe4fa416696d8f830054c40a2f6dbb0dac014
singcl <24661881@qq.com>
2019-03-03 19:13:14 +0800

    :art: AppTop组件构建

--- src/components/AppTop.js
+++ src/components/AppTop.js
@@ -13,13 +13,13 @@ import React, { PureComponent } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { NavigationScreenProp } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Feather'
-// import LinearGradient from 'react-native-linear-gradient'
+import LinearGradient from 'react-native-linear-gradient'
 import { BorderlessButton } from 'react-native-gesture-handler'
 
-type Props = {
-    title: String,
-    themeColor: Array<String>,
-    navigation: NavigationScreenProp,
+interface Props {
+    title: String;
+    themeColor: Array<String>;
+    navigation: NavigationScreenProp;
 }
 
 export default class AppTop extends PureComponent<Props> {
@@ -32,8 +32,45 @@ export default class AppTop extends PureComponent<Props> {
         const { title, themeColor, children } = this.props
         return (
             <View>
-                <Text>{title}</Text>
+                <LinearGradient
+                    colors={themeColor.length > 1 ? themeColor : [...themeColor, ...themeColor]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={styles.appBar}
+                >
+                    <BorderlessButton activeOpacity={0.8} style={styles.btn} onPress={this.handleOnPress}>
+                        <Icon name="menu" size={20} color="#fff" />
+                    </BorderlessButton>
+                </LinearGradient>
+
+                <Text style={styles.appTitle} numberOfLines={1}>
+                    {title}
+                </Text>
+                {children || null}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    appBar: {
+        paddingTop: $.STATUS_HEIGHT,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    btn: {
+        width: 48,
+        height: 48,
+        zIndex: 1,
+        backgroundColor: 'rgba(0,0,0,0)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    appTitle: {
+        flex: 1,
+        fontSize: 16,
+        color: '#fff',
+    },
+})
