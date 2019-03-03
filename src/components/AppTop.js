9f2ff6dd9086031bc41a65afd8a40658c1aa74cd
singcl <24661881@qq.com>
2019-03-04 01:02:20 +0800

    :art: declare var $: any  In flow.

--- src/components/AppTop.js
+++ src/components/AppTop.js
@@ -9,21 +9,23 @@
  * @see https://github.com/singcl
  */
 
-import React, { PureComponent } from 'react'
+// import React, { PureComponent } from 'react'
+import * as React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { NavigationScreenProp } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Feather'
 import LinearGradient from 'react-native-linear-gradient'
 import { BorderlessButton } from 'react-native-gesture-handler'
 
+declare var $: any
 interface Props {
-    title: String;
-    themeColor: Array<String>;
+    title: string;
+    themeColor: Array<string>;
     navigation: NavigationScreenProp;
-    children: React.ReactNode;
+    children: React.Node;
 }
 
-export default class AppTop extends PureComponent<Props> {
+export default class AppTop extends React.PureComponent<Props> {
     handleOnPress = () => {
         const { navigation } = this.props
         navigation.openDrawer()
