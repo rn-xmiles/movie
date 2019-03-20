c6ee52b98ed43b5a4695650929b1c6a9e4fd64be
singcl <24661881@qq.com>
2019-03-21 00:23:39 +0800

    :art: AsyncStorage KV数据本地持久化

---
+++ src/store/storage.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ * @author singcl <iambabyer@gmail.com>
+ * @see https://github.com/singcl
+ */
+
+import { AsyncStorage } from 'react-native'
+
+class Storage {
+    /**
+     * 获取
+     */
+    static get = async (key) => {
+        try {
+            const value = await AsyncStorage.getItem(key)
+            if (value !== null) {
+                return JSON.parse(value)
+            } else {
+                return false
+            }
+        } catch (error) {
+            return false
+        }
+    }
+
+    /**
+     * 保存
+     */
+    static save = async (key, value) => {
+        try {
+            await AsyncStorage.setItem(key, JSON.stringify(value))
+            return true
+        } catch (error) {
+            // 保存失败
+            return false
+        }
+    }
+
+    /**
+     * 更新
+     */
+    static update = async (key, value) => {
+        try {
+            await AsyncStorage.mergeItem(key, JSON.stringify(value))
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+
+    /**
+     * 删除
+     */
+    static delete = async (key) => {
+        try {
+            await AsyncStorage.removeItem(key)
+            return true
+        } catch (error) {
+            // 删除失败
+            return false
+        }
+    }
+}
+
+export default Storage
